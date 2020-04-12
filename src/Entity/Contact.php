5338f8414ac350c5fb89fe4acc159506b1e8c0c3
Ylahjaily <y.lahjaily@ecole-ipssi.net>
2020-04-12 16:57:06 +0200

    added Contact entity

---
+++ src/Entity/Contact.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phoneNumber;
+
+    public function __construct(String $lastName,String $firstName, String $email,String $phoneNumber)
+    {
+        $this->lastName = $lastName;
+        $this->firstName = $firstName;
+        $this->email = $email;
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    public function getId() : ? int
+    {
+        return $this->id;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): void
+    {
+        $this->phoneNumber = $phoneNumber;
+    }
+}
--- tests/Entity/ContactTest.php
+++ tests/Entity/ContactTest.php
@@ -3,8 +3,74 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Contact;
+use PHPUnit\Framework\TestCase;
 
-class ContactTest
+class ContactTest extends TestCase
 {
+    public function testConstructor()
+    {
+        $contact = new Contact("lastname","firstname","email@email.fr","0750933920");
+        static::assertSame(
+            "lastname",
+            $contact->getLastName()
+        );
+    }
 
+    public function testConstructorWithEmptyString()
+    {
+        $contact = new Contact("","","","");
+        static::assertEmpty(
+            $contact->getLastName()
+        );
+    }
+
+    public function testGetId()
+    {
+        $contact = new Contact("","","","");
+        static::assertSame(
+            null,
+            $contact->getId()
+        );
+    }
+
+    public function testSetLastname()
+    {
+        $contact = new Contact("","","","");
+        $contact->setLastName("lastname");
+        static::assertSame(
+            "lastname",
+            $contact->getLastname()
+        );
+    }
+
+    public function testSetFirstName()
+    {
+        $contact = new Contact("","","","");
+        $contact->setFirstName("firstname");
+        static::assertSame(
+            "firstname",
+            $contact->getFirstName()
+        );
+    }
+
+    public function testSetEmail()
+    {
+        $contact = new Contact("","","","");
+        $contact->setEmail("email@email.fr");
+        static::assertSame(
+            "email@email.fr",
+            $contact->getEmail()
+        );
+    }
+
+    public function testSetPhoneNumber()
+    {
+        $contact = new Contact("","","","");
+        $contact->setPhoneNumber("0750933920");
+        static::assertSame(
+            "0750933920",
+            $contact->getPhoneNumber()
+        );
+    }
 }
